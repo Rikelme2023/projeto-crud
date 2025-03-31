3e0f1a66c74389e02eceefe2c4f482f67b10eca6
Rikelme2023 <ruan.silva205@aluno.ce.gov.br>
2025-03-31 20:00:28 -0300

    rota para remover criada

--- backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
@@ -2,17 +2,25 @@ package br.com.crud.produto.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.crud.produto.produtos.modelo.ProdutoModelo;
+import br.com.crud.produto.produtos.modelo.RespostaModelo;
 import br.com.crud.produto.produtos.servico.ProdutoServico;
 
 public class ProdutoControle {
   @Autowired
   private ProdutoServico ps;
+
+  @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
 
   @PostMapping("/cadastrar")
   public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
