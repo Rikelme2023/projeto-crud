d526c3471fdfa34bc9f2105c810a5fb62781e227
Rikelme2023 <ruan.silva205@aluno.ce.gov.br>
2025-03-31 19:54:05 -0300

    Criação do metodo listar em serviço

--- backend/produtos/src/main/java/br/com/crud/produto/produtos/repositorio/ProdutoRepositorio.java
+++ backend/produtos/src/main/java/br/com/crud/produto/produtos/repositorio/ProdutoRepositorio.java
@@ -1,5 +1,11 @@
 package br.com.crud.produto.produtos.repositorio;
 
-public class ProdutoRepositorio {
-  
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.crud.produto.produtos.modelo.ProdutoModelo;
+
+@Repository
+public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
+
 }
--- backend/produtos/src/main/java/br/com/crud/produto/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/crud/produto/produtos/servico/ProdutoServico.java
@@ -1,5 +1,15 @@
 package br.com.crud.produto.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.crud.produto.produtos.modelo.ProdutoModelo;
+import br.com.crud.produto.produtos.repositorio.ProdutoRepositorio;
+
 public class ProdutoServico {
-  
+  @Autowired
+  private ProdutoRepositorio pr;
+
+  public Iterable<ProdutoModelo> listar() {
+    return pr.findAll();
+
+  }
 }
