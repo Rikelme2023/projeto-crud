f86e9f9d240a163e0c12d727b65f7d77a8fe539d
Rikelme2023 <ruan.silva205@aluno.ce.gov.br>
2025-03-31 19:57:07 -0300

    Rota para cadastrar criada

--- backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.crud.produto.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.crud.produto.produtos.modelo.ProdutoModelo;
 import br.com.crud.produto.produtos.servico.ProdutoServico;
@@ -9,6 +12,12 @@ import br.com.crud.produto.produtos.servico.ProdutoServico;
 public class ProdutoControle {
   @Autowired
   private ProdutoServico ps;
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
+    return ps.cadastrar(pm);
+
+  }
 
   @GetMapping("/listar")
   public Iterable<ProdutoModelo> listar() {
