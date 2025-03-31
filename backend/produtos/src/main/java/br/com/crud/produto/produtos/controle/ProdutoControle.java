e014c8d2d6e6e9ef4571f37987a75208243b126d
Rikelme2023 <ruan.silva205@aluno.ce.gov.br>
2025-03-31 19:55:47 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produto/produtos/controle/ProdutoControle.java
@@ -1,5 +1,18 @@
 package br.com.crud.produto.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.crud.produto.produtos.modelo.ProdutoModelo;
+import br.com.crud.produto.produtos.servico.ProdutoServico;
+
 public class ProdutoControle {
-  
+  @Autowired
+  private ProdutoServico ps;
+
+  @GetMapping("/listar")
+  public Iterable<ProdutoModelo> listar() {
+    return ps.listar();
+
+  }
 }
